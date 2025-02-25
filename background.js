d6c02f5ac7fa6edb0ee72cb801ab7df30367f7c7
Victor Joulin-Batejat <victorjb2015@gmail.com>
2025-02-25 12:26:57 -0800

    client id set and background modified accordingly

--- background.js
+++ background.js
@@ -1,16 +1,23 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.identity.launchWebAuthFlow(
-    {
-      url: "https://accounts.google.com/o/oauth2/auth?client_id=YOUR_CLIENT_ID&response_type=token&redirect_uri=https://YOUR_EXTENSION_ID.chromiumapp.org&scope=https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/youtube.upload",
-      interactive: true
-    },
-    (redirectUrl) => {
-      if (chrome.runtime.lastError) {
-        console.error("OAuth failed:", chrome.runtime.lastError);
-        return;
-      }
-      const token = new URL(redirectUrl).hash.split("&")[0].split("=")[1];
-      chrome.storage.local.set({ accessToken: token });
-    }
-  );
-});
+fetch(chrome.runtime.getURL("config.json"))
+  .then(response => response.json())
+  .then(config => {
+    const CLIENT_ID = config.client_id;
+
+    chrome.action.onClicked.addListener(() => {
+      chrome.identity.launchWebAuthFlow(
+        {
+          url: `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&response_type=token&redirect_uri=https://${chrome.runtime.id}.chromiumapp.org&scope=https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/youtube.upload`,
+          interactive: true
+        },
+        (redirectUrl) => {
+          if (chrome.runtime.lastError) {
+            console.error("OAuth failed:", chrome.runtime.lastError);
+            return;
+          }
+          const token = new URL(redirectUrl).hash.split("&")[0].split("=")[1];
+          chrome.storage.local.set({ accessToken: token });
+        }
+      );
+    });
+  })
+  .catch(error => console.error("Failed to load config:", error));
